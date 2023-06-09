206bfed2841caebe1ce5df4aa75ca435914c1f2f
Simon Reinisch <simon@reinisch.io>
2023-06-09 19:03:57 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func GetEnv(key string) string {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func main() {
+
+	// Set mode
+	gin.SetMode(GetEnv("GIN_MODE"))
+
+	// Create router
+	router := gin.New()
+
+	// Configure and start server
+	router.SetTrustedProxies(nil)
+	router.Run("localhost:8080")
+}
